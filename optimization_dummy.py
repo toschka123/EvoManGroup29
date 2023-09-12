3e105b7db779168707f90f5ee70f44834d5f61a9
toschka123 <97736572+toschka123@users.noreply.github.com>
2023-09-12 15:02:52 +0200

    very first changes

--- optimization_dummy.py
+++ optimization_dummy.py
@@ -10,7 +10,7 @@ import sys
 
 from evoman.environment import Environment
 from demo_controller import player_controller
-
+from random import randint, random
 # imports other libs
 import numpy as np
 import os
@@ -25,36 +25,59 @@ def evaluate(env, x):
     return np.array(list(map(lambda y: simulation(env,y), x)))
 
 
-def main():
-    # choose this for not using visuals and thus making experiments faster
-    headless = True
-    if headless:
-        os.environ["SDL_VIDEODRIVER"] = "dummy"
+
+# choose this for not using visuals and thus making experiments faster
+headless = True
+if headless:
+    os.environ["SDL_VIDEODRIVER"] = "dummy"
 
 
-    experiment_name = 'optimization_test'
-    if not os.path.exists(experiment_name):
-        os.makedirs(experiment_name)
+experiment_name = 'optimization_test'
+if not os.path.exists(experiment_name):
+    os.makedirs(experiment_name)
 
-    n_hidden_neurons = 10
+n_hidden_neurons = 10
 
-    # initializes simulation in individual evolution mode, for single static enemy.
-    env = Environment(experiment_name=experiment_name,
-                    enemies=[2],
-                    playermode="ai",
-                    player_controller=player_controller(n_hidden_neurons), # you  can insert your own controller here
-                    enemymode="static",
-                    level=2,
-                    speed="fastest",
-                    visuals=False)
-
-
-    # number of weights for multilayer with 10 hidden neurons
-    n_vars = (env.get_num_sensors()+1)*n_hidden_neurons + (n_hidden_neurons+1)*5
-
-    # start writing your own code from here
+# initializes simulation in individual evolution mode, for single static enemy.
+env = Environment(experiment_name=experiment_name,
+                enemies=[2],
+                playermode="ai",
+                player_controller=player_controller(n_hidden_neurons), # you  can insert your own controller here
+                enemymode="static",
+                level=2,
+                speed="fastest",
+                visuals=False)
 
 
+# number of weights for multilayer with 10 hidden neurons
+n_vars = (env.get_num_sensors()+1)*n_hidden_neurons + (n_hidden_neurons+1)*5
 
-if __name__ == '__main__':
-    main()
+# start writing your own code from here
+
+pop_size = 100
+max_f =-1
+avg_f =-1
+low_f = 999
+
+pop = np.random.uniform(-1, 1, (pop_size, n_vars))
+pop_f = evaluate(env,pop)
+max_f=max(pop_f)
+avg_f = sum(pop_f)/len(pop_f)
+low_f = min(pop_f)
+
+print(max_f, avg_f)
+
+def recombination(i1, i2): #Takes as input two parents and returns 2 babies, in each position 50% chance to have parent1's gene
+    baby1=[]
+    baby2=[]
+    for i in range(len(i1)):
+        if randint(0,1) == 1:
+            baby1.append(i1[i])
+            baby2.append(i2[i])
+        else:
+            baby1.append(i2[i])
+            baby2.append(i1[i])
+    return baby1, baby2
+
+def parent_selection(population):
+    
