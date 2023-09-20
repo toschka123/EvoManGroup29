62e4c3fea151a4b10366809221d83d5b62baa85b
guoda23 <paulauskaite.guoda@gmail.com>
2023-09-20 16:57:20 +0200

    created (mu, lambda) survivor selector and made gaussian mutation neater

--- optimization_dummy.py
+++ optimization_dummy.py
@@ -26,7 +26,6 @@ def evaluate(env, x):
     return np.array(list(map(lambda y: simulation(env,y), x)))
 
 
-
 # choose this for not using visuals and thus making experiments faster
 headless = True
 if headless:
@@ -61,8 +60,10 @@ avg_f =-1
 low_f = 999
 maxGens=20
 Gen=0
-N_newGen=pop_size # define how many offsprings we want to produce and how many old individuals we want to kill NOTE This has to be even!!
+# N_newGen=pop_size # define how many offsprings we want to produce and how many old individuals we want to kill NOTE This has to be even!!
 mutation_strength = 0.04
+fitness_survivor_no = 2 # how many children in the new generation will be from "best". The rest are random.
+gaussian_mutation_sd = 0.5 
 
 
 pop = np.random.uniform(-1, 1, (pop_size, n_vars)) #initialize population
@@ -98,15 +99,12 @@ def mutate(individual):
 
 
 def mutate_gene_gaussian(gene):
-    mutation = np.random.normal(0, 0.5)
+    mutation = np.random.normal(0, gaussian_mutation_sd)
 
-    if mutation + gene > 1: #if values too big
-        return 0.99
-    elif mutation < -1: #if values too small
-        return -0.99
+    while (mutation + gene > 1) or (mutation + gene < -1):
+        mutation = np.random.normal(0, gaussian_mutation_sd)
 
     gene += mutation
-
     return gene
 
 
@@ -120,8 +118,22 @@ def mutate_gene_gaussian(gene):
         list_of_parents.append([p1, p2])
     return list_of_parents"""
 
-def parent_selection(population, f_values, tournament_size=2, N_newGen = N_newGen/2): #Generate Pairs of parents and return them
-    num_parents = N_newGen
+# def parent_selection(population, f_values, tournament_size=2, N_newGen = N_newGen/2): #Generate Pairs of parents and return them
+#     num_parents = N_newGen
+#     selected_parents = []
+
+#     for _ in range(num_parents):
+#         tournament_indices = np.random.choice(num_parents, size=tournament_size, replace=False)
+#         tournament_individuals = [population[i] for i in tournament_indices]
+#         tournament_fitness = [f_values[i] for i in tournament_indices]
+
+#         # Choose the best individual from the tournament as the parent
+#         best_index = np.argmax(tournament_fitness)
+#         selected_parents.append(tournament_individuals[best_index])
+#     return selected_parents
+
+def parent_selection(population, f_values, tournament_size):
+    num_parents = len(population)
     selected_parents = []
 
     for _ in range(num_parents):
@@ -132,7 +144,9 @@ def parent_selection(population, f_values, tournament_size=2, N_newGen = N_newGe
         # Choose the best individual from the tournament as the parent
         best_index = np.argmax(tournament_fitness)
         selected_parents.append(tournament_individuals[best_index])
+
     return selected_parents
+
 
 def kill_people(population, howManyShouldDie): #kill random individual
     choiceInd = random.sample(range(0,len(population)), howManyShouldDie)
@@ -143,6 +157,24 @@ def select_surv(pop, f_pop, N_remove=N_newGen):
     survivors = []
     for i in indxs:
         survivors.append(pop[i])
+    return survivors
+
+
+# Returns a survivor array containing surviving children (only!).
+# Some (small) number of surviving children are picked based on fitness.
+# The rest are picked randomly.
+def survivor_selector_mu_lambda(children, no_best_picks):
+    survivors = np.random.uniform(-1, 1, (pop_size, n_vars)) #preallocate a random array for survivors
+
+    children_fitness = evaluate(env, children)
+    indices_best_children = np.argpartition(children_fitness, -no_best_picks)[-no_best_picks:]
+
+    for i in range(no_best_picks): #add some number of best children to the new population
+        survivors[i] =children[indices_best_children[i]] 
+    
+    for i in range(no_best_picks, pop_size): #fill the rest of the population with random children
+        survivors[i] = children[random.randint(0, pop_size-1)]
+
     return survivors
 
 
@@ -171,27 +203,17 @@ def mutate(individual):
 
 while Gen < maxGens:
     parents=[]
-    for i in range(int(N_newGen/2)): #Choose how many pairs of parents we want
-        parents.append(parent_selection(pop, pop_f,4, N_newGen))
-    new_kids = []
-    for pairs in parents: #Each pair of parents generates two offspring
-        baby1, baby2 = recombination(pairs[0], pairs[1])
-        new_kids.append(baby1)
-        new_kids.append(baby2)
+    parents = parent_selection(pop, pop_f, 4) #generates 100 parents - parent selection seems to make the convergion faster
 
-    #This is combining new and old and
-    """old_generation = pop.tolist()
-    total = old_generation + new_kids
-    total =np.array(total)
-    total_f = evaluate(env, total)
-    survivors = select_surv(total, total_f, 100)
-    for i in range(len(survivors)):
-        pop[i] = survivors[i]"""
+    new_kids = np.random.uniform(-1, 1, (6*pop_size, n_vars)) #preallocate 600 kids
+    for i in range(0,len(new_kids),2):
+        baby1, baby2 = recombination(parents[random.randint(0, 99)], parents[random.randint(0, 99)])
+        new_kids[i] = baby1
+        new_kids[i+1] = baby2
 
-    #This is without survivor select - age based
-    inds = kill_people(pop, N_newGen) # pick how many individuals we want to kill
-    for i in range(len(inds)):
-        pop[inds[i]] = new_kids[i]
+    survivors = survivor_selector_mu_lambda(new_kids, fitness_survivor_no)
+    for i in range(pop_size):
+        pop[i] = survivors[i]
 
 
     Gen+=1
@@ -199,7 +221,7 @@ while Gen < maxGens:
     max_f = max(pop_f)
     avg_f = sum(pop_f) / len(pop_f)
     low_f = min(pop_f)
-    print(max_f, avg_f,len(pop))
+    print(max_f, avg_f)
 
 
 
