0523402173bb8bcbbd9a0320a5988845328c797c
KasperCastelijn <81652558+KasperCastelijn@users.noreply.github.com>
2023-10-01 18:06:56 +0200

    Add files via upload

--- adaptive.py
+++ adaptive.py
@@ -18,22 +18,25 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt 
 
-# runs simulation
-def simulation(env,x):
-    f,p,e,t = env.play(pcont=x)
+
+# Runs simulation
+def simulation(env, x):
+    f, p, e, t = env.play(pcont=x)
     return f
 
-# evaluation
+
+# Evaluation
 def evaluate(env, x):
-    return np.array(list(map(lambda y: simulation(env,y), x)))
+    return np.array(list(map(lambda y: simulation(env, y), x)))
 
 
-#Function that returns the population with only the 265 weights, no sigma
+# Returns the population with only the 265 weights, no sigma
 def pop_weights_only(pop):
     weights_only = pop[:,1:]
     return weights_only
 
-# choose this for not using visuals and thus making experiments faster
+
+# Choose this for not using visuals and thus making experiments faster
 headless = True
 if headless:
     os.environ["SDL_VIDEODRIVER"] = "dummy"
@@ -44,63 +47,62 @@ if not os.path.exists(experiment_name):
 
 n_hidden_neurons = 10
 
-# initializes simulation in individual evolution mode, for single static enemy.
+# Initializes simulation in individual evolution mode, for single static enemy.
 env = Environment(experiment_name=experiment_name,
                 enemies=[7],
                 playermode="ai",
-                player_controller=player_controller(n_hidden_neurons), # you  can insert your own controller here
+                player_controller=player_controller(n_hidden_neurons),  # You  can insert your own controller here
                 enemymode="static",
                 level=2,
                 speed="fastest",
                 visuals=False)
 
 
-# number of variables for multilayer with 10 hidden neurons (265) plus one sigma value
+# Number of variables for multilayer with 10 hidden neurons (265) plus one sigma value
 n_vars = (env.get_num_sensors()+1)*n_hidden_neurons + (n_hidden_neurons+1)*5 +1
 
-# start writing your own code from here
+# Start writing your own code from here
+
+# Initialization
+run_mode = "train"
 pop_size = 100
 max_f = -1
 avg_f = -1
 low_f = 999
 maxGens = 20
 Gen = 0
-N_newGen = pop_size * 4  # define how many offsprings we want to produce and how many old individuals we want to kill NOTE This has to be even!!
-mutation_threshold = 0.04
-fitness_survivor_no = 20  # how many children in the new generation will be from "best". The rest are random.
+N_newGen = pop_size * 4  # Define number of offspring generated and old individuals to kill (This has to be even!!)
+mutation_threshold = 0.04  # Increasing lowers the chance of mutation
+fitness_survivor_no = 20  # How many children in the new generation will be from "best". The rest are random.
 gaussian_mutation_sd = 0.5
 overall_best = -1
 fitness_avg_history = []
 fitness_best_history = []
 fitness_history = []
 
-#Generate 266 genes, at loc 0 we find the sigma, the rest of the array is the weights
-pop = np.random.uniform(-1, 1, (pop_size, n_vars)) #Initialize population, with extra value for the weights
+# Initialise the population, with each individual having 265 genes(weights) and one extra for the sigma
+pop = np.random.uniform(-1, 1, (pop_size, n_vars))
 
-#Define the bounds of your initial sigma values and tao for self-adaptive mutation
+# Define bounds for the initial sigma values and tau for self-adaptive mutation
 sigma_i_U = 0.1
 sigma_i_L = 0.01
-tao = 0.05
+tau = 0.05
 
-#Generate the initial sigma values and place them at location 0 for each individual array
+# Generate the initial sigma values and place them at index 0 for each individual
 sigma_vals_i = [random.uniform(sigma_i_U,sigma_i_L) for individual in range(pop_size)]
 pop[:, 0] = sigma_vals_i
 avg_sigma_start = sum(sigma_vals_i)/len(sigma_vals_i)
 
-#Evaluate population
+# Evaluate initial population
 pop_f = evaluate(env,pop_weights_only(pop))
-#max_f = max(pop_f)
-#avg_f = sum(pop_f)/len(pop_f)
-#low_f = min(pop_f)
-#print(max_f, avg_f)
-run_mode = "train"
 
-#Uniform recombination
-def uniform_recombination(i1, i2): #Takes as input two parents and returns 2 babies, in each position 50% chance to have parent1's gene
-    baby1=[]
-    baby2=[]
 
-    #Start with choosing which sigma to inherit
+# Takes two parents and returns 2 babies with each gene having a 50% chance to be from either parent
+def uniform_recombination(i1, i2):
+    baby1 = []
+    baby2 = []
+
+    # Start with choosing which sigma to inherit
     if randint(0, 1) == 1:
         baby1.append(i1[0])
         baby2.append(i2[0])
@@ -108,26 +110,26 @@ def uniform_recombination(i1, i2): #Takes as input two parents and returns 2 bab
         baby2.append(i1[0])
         baby1.append(i2[0])
 
-    #Then possibly perform mutation on either the sigma of your first or your second baby
+    # Decide if a baby will mutate
     if random.random() > mutation_threshold:
 
-        #Generate the stepsize (mutation size) of your sigma value
-        step_size = math.e ** (tao * np.random.normal(0, 1))
+        # Generate the mutation size for the sigma value
+        mutation_size = math.e ** (tau * np.random.normal(0, 1))
 
-        #Decide which baby to mutate and assign it its new sigma
+        # Decide which baby to mutate, multiplying its old sigma with the mutation size
         if randint(0, 1) == 1:
-            sigma_prime = baby1[0] * step_size
+            sigma_prime = baby1[0] * mutation_size
             baby1[0] = sigma_prime
 
         else:
-            sigma_prime = baby2[0] * step_size
+            sigma_prime = baby2[0] * mutation_size
             baby2[0] = sigma_prime
 
     sigma1 = baby1[0]
     sigma2 = baby2[0]
 
-    for i in range(1,len(i1)):
-        if randint(0,1) == 1:
+    for i in range(1, len(i1)):
+        if randint(0, 1) == 1:
             baby1.append(i1[i])
             baby2.append(i2[i])
         else:
@@ -139,19 +141,19 @@ def uniform_recombination(i1, i2): #Takes as input two parents and returns 2 bab
         if random.random() > mutation_threshold:
             baby2[i] = mutate_gene_sa(baby1[i], sigma2)
 
-
     return baby1, baby2
 
 
+# Mutates a single gene
 def mutate_gene_sa(gene, s):
-    #Only perform mutation when result stays within weight range (-1,1)
-    mutation = s * np.random.normal(0,1)
-    while abs(gene + mutation) > 1:
-        mutation = s * np.random.normal(0,1)
+    mutation = s * np.random.normal(0, 1)
+    while abs(gene + mutation) > 1:  # Ensures mutations stay between (-1, 1)
+        mutation = s * np.random.normal(0, 1)
     gene += mutation
     return gene
 
 
+# Alternative way of mutation (not in use)
 def mutate_gene_gaussian(gene):
     mutation = np.random.normal(0, gaussian_mutation_sd)
 
@@ -161,9 +163,11 @@ def mutate_gene_gaussian(gene):
     gene += mutation
     return gene
 
+
+# Tournament that decides which parents should create the new generation
 def adaptive_tournament_selection(population, f_values, min_tournament_size=2, max_tournament_size=5):
     num_parents = int(len(population)/2)
-    selected_parents = []  # List to store the selected parents
+    selected_parents = []
 
     # Track the diversity of individuals using an array of zeros
     diversity_scores = np.zeros(num_parents)
@@ -190,66 +194,60 @@ def adaptive_tournament_selection(population, f_values, min_tournament_size=2, m
         selected_parents.append(population[best_index1])
         selected_parents.append(population[best_index2])
 
-    #print(len(selected_parents))
     return selected_parents  # Return the list of selected parents
 
 
-# Returns a survivor array containing surviving children (only!).
-# Some (small) number of surviving children are picked based on fitness.
-# The rest are picked randomly.
+# Decides which children survive and returns them in an array
 def survivor_selector_mu_lambda(children, no_best_picks):
-    survivors = np.random.uniform(-1, 1, (pop_size, n_vars)) #preallocate a random array for survivors
+
+    survivors = np.random.uniform(-1, 1, (pop_size, n_vars))  # Preallocate a random array for survivors
 
     children_without_sigma = pop_weights_only(children)
-
     children_fitness = evaluate(env, children_without_sigma)
-
+    # Ranks the children based on their fitness value
     indices_best_children = np.argpartition(children_fitness, -no_best_picks)[-no_best_picks:]
 
-    for i in range(no_best_picks): #add some number of best children to the new population
+    for i in range(no_best_picks):  # Adds the best children to the new population
         survivors[i] = children[indices_best_children[i]]
     
-    for i in range(no_best_picks, pop_size): #fill the rest of the population with random children
+    for i in range(no_best_picks, pop_size):  # Fills remaining population with random children
         survivors[i] = children[random.randint(0, pop_size-1)]
 
     return survivors
 
-if run_mode =='test':
+
+if run_mode == 'test':
 
     bsol = np.loadtxt(experiment_name+'/best.txt')
-    print( '\n RUNNING SAVED BEST SOLUTION \n')
-    env.update_parameter('speed','normal')
+    print('\n RUNNING SAVED BEST SOLUTION \n')
+    env.update_parameter('speed', 'normal')
     env.update_parameter('visuals', True)
     evaluate(env, [bsol])
 
     sys.exit(0)
 
 elif run_mode == 'train':
-    for RunNumber in range(10):
+
+    for run_number in range(10):
+        # Resets the initial parameters
         pop_size = 100
-        max_f = -1
-        avg_f = -1
-        low_f = 999
         maxGens = 20
         Gen = 0
-        N_newGen = pop_size * 4  # define how many offsprings we want to produce and how many old individuals we want to kill NOTE This has to be even!!
+        N_newGen = pop_size * 4
         mutation_threshold = 0.04
-        fitness_survivor_no = 20  # how many children in the new generation will be from "best". The rest are random.
+        fitness_survivor_no = 20
         gaussian_mutation_sd = 0.5
         overall_best = -1
         fitness_avg_history = []
         fitness_best_history = []
         fitness_history = []
 
-        # Generate 266 genes, at loc 0 we find the sigma, the rest of the array is the weights
-        pop = np.random.uniform(-1, 1, (pop_size, n_vars))  # Initialize population, with extra value for the weights
+        pop = np.random.uniform(-1, 1, (pop_size, n_vars))
 
-        # Define the bounds of your initial sigma values and tao for self-adaptive mutation
         sigma_i_U = 0.1
         sigma_i_L = 0.01
-        tao = 0.05
+        tau = 0.05
 
-        # Generate the initial sigma values and place them at location 0 for each individual array
         sigma_vals_i = [random.uniform(sigma_i_U, sigma_i_L) for individual in range(pop_size)]
         pop[:, 0] = sigma_vals_i
         avg_sigma_start = sum(sigma_vals_i) / len(sigma_vals_i)
@@ -260,39 +258,32 @@ elif run_mode == 'train':
         avg_f = sum(pop_f) / len(pop_f)
         low_f = min(pop_f)
         print(max_f, avg_f)
+
+        # Run once for each generation
         while Gen < maxGens:
-            # parents = []
-            # for i in range(int(N_newGen/2)):
-            #     parents.append(adaptive_tournament_selection(pop, pop_f, 6, N_newGen))
 
-            parents=[]
-            parents = adaptive_tournament_selection(pop, pop_f, 4) #generates 100 parents - parent selection seems to make the convergion faster
-            new_kids = np.random.uniform(-1, 1, (N_newGen, n_vars)) #preallocate 600 kids
+            parents = adaptive_tournament_selection(pop, pop_f, 4)  # Generates parents
+            new_kids = np.random.uniform(-1, 1, (N_newGen, n_vars))  # Preallocate kids
 
-            for i in range(0,N_newGen,2):
+            # Generate the new kids
+            for i in range(0, N_newGen, 2):
                 baby1, baby2 = uniform_recombination(parents[i], parents[i+1])
                 new_kids[i] = baby1
                 new_kids[i + 1] = baby2
 
-
-            """if len(new_kids) > 100:
-                for i in range(0,len(new_kids)-100, 2):
-                    baby1, baby2 = uniform_recombination(parents[randint(0,99)], parents[randint(0,99)])
-                    new_kids[i+100] = baby1
-                    new_kids[i+101] = baby2"""
-
-            #print(f"new_kids: {new_kids}")
-            #print(f"shape: {len(new_kids), len(new_kids[0])}")
-            #print(f"fitness_survivor_no: {fitness_survivor_no}")
+            # Decide which kids survive
             survivors = survivor_selector_mu_lambda(new_kids, fitness_survivor_no)
+
+            # Overwrite the old population with the new survivors generation
             for i in range(pop_size):
                 pop[i] = survivors[i]
 
-            Gen+=1
+            Gen += 1
 
             pop_without_sigma = pop_weights_only(pop)
 
-            pop_f = evaluate(env,pop_without_sigma) #evaluate new population
+            # evaluate new population
+            pop_f = evaluate(env, pop_without_sigma)
             max_f = max(pop_f)
             avg_f = sum(pop_f) / len(pop_f)
             low_f = min(pop_f)
@@ -300,12 +291,11 @@ elif run_mode == 'train':
 
             fitness_avg_history.append((avg_f))
 
-
+            # Saves the best individual
             if max_f > overall_best:
                 overall_best = max_f
                 best = np.argmax(pop_f)
                 best_individual = pop_without_sigma[best]
-                overall_best = max_f
 
                 np.savetxt(experiment_name + '/best.txt', pop_without_sigma[best])
 
@@ -313,20 +303,20 @@ elif run_mode == 'train':
             # Calculate and store the fitness values of the current population
             fitness_values = evaluate(env, pop_without_sigma)
             fitness_history.append(fitness_values)
-
             # Calculate the standard deviation of fitness values
             fitness_std = np.std(fitness_values)
 
-            # Print or log the fitness diversity metric for the current generation
+            # Print the fitness diversity metric for the current generation
             print(f"Generation {Gen}: Fitness Diversity (Std Dev): {fitness_std}")
 
         avg_sigma_end = sum(pop[:,0])/len(pop[:,0])
-        save_run(fitness_avg_history, fitness_best_history, avg_sigma_start, avg_sigma_end, "waterman", RunNumber)
-        # After the loop, you can visualize the fitness diversity over generations if needed
-        #plt.plot(range(maxGens), [np.std(fitness) for fitness in fitness_history])
-        #plt.title("Fitness Diversity Over Generations")
-        #plt.xlabel("Generation")
-        #plt.ylabel("Standard Deviation of Fitness")
-        #plt.show()
+        save_run(fitness_avg_history, fitness_best_history, avg_sigma_start, avg_sigma_end, "waterman", run_number)
+
+        # Optionally visualize the fitness diversity over generations
+        plt.plot(range(maxGens), [np.std(fitness) for fitness in fitness_history])
+        plt.title("Fitness Diversity Over Generations")
+        plt.xlabel("Generation")
+        plt.ylabel("Standard Deviation of Fitness")
+        plt.show()
 
 
